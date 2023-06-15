583493d76bc2fb6bc69bf36f25fc6553ac9470a6
abhinav-bohra <abhinavbohra@iitkgp.ac.in>
2023-06-16 01:46:31 +0530

    New Version

---
+++ assets/js/loader.js
@@ -0,0 +1,7 @@
+$(function(){
+ $("#header").load("header.html");
+ $("#sidebar").load("sidebar.html");
+ $("#footer").load("footer.html");
+});
+
+document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/g, '') + ' js ';
